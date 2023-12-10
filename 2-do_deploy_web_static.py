1b6076e849f9aa6824084e0df8c0c1f2ea400d01
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-12-11 00:00:07 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,7 +36,7 @@ def do_deploy(archive_path):
         run(f"rm /tmp/{src}.tgz")
         run(f"mv {root}/{src}/web_static/* {root}/{src}/")
         run(f"rm -rf {root}/{src}/web_static ")
-        run("rm -rf /data/web_static/current")
+        # run("rm -rf /data/web_static/current")
         run(f"ln -s {root}/{src} /data/web_static/current")
         print("New version deployed!")
 

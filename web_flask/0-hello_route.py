53486d69acfa4d08f519314710f10786da8010e3
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-28 19:23:52 +0100

    requirement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""0. Hello Flask!"""
+
 from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
-def hello_world():
+def hello():
+    """print hello"""
     return "Hello HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
